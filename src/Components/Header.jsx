35256d3d29fe8d048bff6d1840ef45184bf91225
JoaoVitorCarottaRibeiro <joaovitcr@gmail.com>
2024-11-14 16:07:52 -0300

    começo gs

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,21 @@
-export default function Header() {
-    return(
-        <>
-            <h1>Header</h1>
-        </>
-    )
+import Econect from '../../public/Imagens/Econect-sem-fundo.png';
+import { Link } from 'react-router-dom';
+
+export default function Header({}) {
+  return (
+    <>
+      <nav className="w-full p-20 py-6 bg-slate-600 flex justify-center items-center">
+        <div className="flex justify-between items-center w-full max-w-7xl">
+          <img className="w-32 h-auto" src={Econect} alt="Logo Econect" />
+          <div className="text-lg ml-auto flex space-x-8">
+            <Link to="/" className="hover:underline">Home</Link>
+            <Link to="/sobre" className="hover:underline">Sobre</Link>
+            <Link to="/simulacao" className="hover:underline">Simulação</Link>
+            <Link to="/servicos" className="hover:underline">Serviços</Link>
+            <Link to="/contato" className="hover:underline">Contato</Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
